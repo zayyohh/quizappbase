2fe5f24dcb5b6e2bbdfb17868529b9bef61834db
thiago waldeck <thiagowsvianna@gmail.com>
2024-06-20 00:25:47 +0000

    alteração dinamica

--- pages/Quiz/Quiz.js
+++ pages/Quiz/Quiz.js
@@ -1,11 +1,26 @@
 import { verificarTema, trocarTema } from "../../helpers/tema-helper";
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
-
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
+@@ -8,4 +8,19 @@ botaoTema.addEventListener("click", () => {
 })
 
 
 verifivarTema (body, botaoTema)
+verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+function alternarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `icone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alternarAssunto()

0a79b1934e1e79830ae07ce6de7e28641f5234a0
thiago waldeck <thiagowsvianna@gmail.com>
2024-06-20 00:30:09 +0000

    função de buscar as perguntas

--- pages/Quiz/Quiz.js
+++ pages/Quiz/Quiz.js
@@ -1,26 +1,111 @@
-import { verificarTema, trocarTema } from "../../helpers/tema-helper";
 import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
-@@ -8,4 +8,19 @@ botaoTema.addEventListener("click", () => {
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
 })
+const botaoTema = document.querySelector(".tema button");
+const body = document.querySelector("body");
+const assunto = localStorage.getItem("assunto");
 
+let quiz = {};
+let pontos = 0;
+let pergunta = 1;
 
-verifivarTema (body, botaoTema)
 verificarTema(body, botaoTema)
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema);
+});
 
 const assunto = localStorage.getItem("assunto")
+verificarTema(body, botaoTema);
 
 function alternarAssunto() {
     const divIcone = document.querySelector(".assunto_icone")
     const iconeImg = document.querySelector(".assunto_icone img")
+    const divIcone = document.querySelector(".assunto_icone");
+    const iconeImg = document.querySelector(".assunto_icone img");
     const assuntoTitulo = document.querySelector(".assunto h1")
 
     divIcone.classList.add(assunto.toLowerCase())
     iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLocaleLowerCase()}.svg`)
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
     iconeImg.setAttribute("alt", `icone de ${assunto}`)
     assuntoTitulo.innerText = assunto
 }
 
 alternarAssunto()
+async function buscarPerguntas() {
+    const urlDados = "../../data.json"
+
+    await fetch(urlDados)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            dados.quizzes.forEach(dado => {
+                if (dado.title === assunto) {
+                    quiz = dado;
+                }
+            })
+        })
+}
+
+function montarPergunta() {
+    const main = document.querySelector("main");
+
+    main.innerHTML = `
+        <section class="pergunta">
+            <div>
+                <p>Questão ${pergunta} de 10</p>
+                <h2>${alterarSinais(quiz.questions[pergunta-1].question)}</h2>
+            </div>
+            <div class="barra_progresso">
+                <div style="width: ${pergunta * 10}%"></div>
+            </div>
+        </section>
+        <section class="alternativas">
+            <form action="">
+                <label for="alternativa_a">
+                    <input type="radio" id="alternativa_a" name="alternativa">
+                    <div>
+                        <span>A</span>
+                        ${alterarSinais(quiz.questions[pergunta-1].options[0])}
+                    </div>
+                </label>
+                <label for="alternativa_b">
+                    <input type="radio" id="alternativa_b" name="alternativa">
+                    <div>
+                        <span>B</span>
+                        ${alterarSinais(quiz.questions[pergunta-1].options[1])}
+                    </div>
+                </label>
+                <label for="alternativa_c">
+                    <input type="radio" id="alternativa_c" name="alternativa">
+                    <div>
+                        <span>C</span>
+                        ${alterarSinais(quiz.questions[pergunta-1].options[2])}
+                    </div>
+                </label>
+                <label for="alternativa_d">
+                    <input type="radio" id="alternativa_d" name="alternativa">
+                    <div>
+                        <span>D</span>
+                        ${alterarSinais(quiz.questions[pergunta-1].options[3])}
+                    </div>
+                </label>
+            </form>
+            <button>Enviar</button>
+        </section>`
+}
+
+function alterarSinais(texto){
+    return texto.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    }
+async function iniciar() {
+    alternarAssunto()
+    await buscarPerguntas()
+    montarPergunta()
+}
+
+iniciar()

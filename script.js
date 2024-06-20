f5fc100dac79114aab9be5c1074fd12abae54f79
thiago waldeck <thiagowsvianna@gmail.com>
2024-06-20 00:12:12 +0000

    adicionada a função para escolher o tema do Quiz

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-let tema = "claro"
 import { trocarTema, verificarTema} from "./helpers/tema-helper.js"
 
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
-
-function verificarTema() {
-    if (localStorage.getItem("tema")){
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema()
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
+    trocarTema(body, botaoTema);
 })
 
+verificarTema(body, botaoTema);
 
 verifivarTema (body, botaoTema)
 
+const botoesAssunto = document.querySelectorAll(".assuntos button");
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerText;
+    localStorage.setItem("assunto", assunto);
+    window.location.href = "./pages/quiz/quiz.html"
+}
